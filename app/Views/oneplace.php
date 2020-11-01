1efe293559c8317f4073b88b780f0424daf8c1fd
ZimingLi6 <2601182984@qq.com>
2020-11-01 15:13:42 +0800

    New add

--- app/Views/oneplace.php
+++ app/Views/oneplace.php
@@ -1,12 +1,13 @@
 <!doctype html>
 <html>
 	<head>
-		<title>DGPT4711 Lab 04</title>
+		<title>lab7</title>
 	</head>
 	<body>
-		<h1>One Travel Destinations</h1>
+		<h1>Golden State Warriors</h1>
 		<p><strong>ID:</strong> {id}</p>
 		<p><strong>Name:</strong> {name}</p>
+                <p><strong>Country:</strong> {Country}</p>
 		<p><strong>Description:</strong> {description}</p>
 		<p><strong>Tourism site:</strong> {link}</p>
 		<p><img src="/image/{image}"/></p>
